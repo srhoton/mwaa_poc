7fbfd237325e883efc11591d505d53644fe5101f
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-11 16:15:15 -0500

    Adjust output

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,7 +1,15 @@
-output "dag_s3_path" {
-  value = module.mwaa.dag_s3_path
+output "test_dag_s3_path" {
+  value = module.test_mwaa.dag_s3_path
 }
 
-output "s3_arn" {
-  value = module.mwaa.s3_arn
+output "test_s3_arn" {
+  value = module.test_mwaa.s3_arn
+}
+
+output "prod_dag_s3_path" {
+  value = module.prod_mwaa.dag_s3_path
+}
+
+output "prod_s3_arn" {
+  value = module.prod_mwaa.s3_arn
 }
