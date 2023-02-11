869e3a7c170f7911e55069a981954e4ae1a53972
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-11 16:10:25 -0500

    Add test and prod environments

--- infrastructure/mwaa.tf
+++ infrastructure/mwaa.tf
@@ -1,5 +1,13 @@
-module "mwaa" {
+module "test_mwaa" {
   source = "git@github.com:srhoton/tf-module-mwaa.git"
   private_subnet_ids = module.default_network.private_subnet_list_mwaa
   vpc_id = module.default_network.vpc_id
+  env_name = "srhoton-test"
+}
+
+module "prod_mwaa" {
+  source = "git@github.com:srhoton/tf-module-mwaa.git"
+  private_subnet_ids = module.default_network.private_subnet_list_mwaa
+  vpc_id = module.default_network.vpc_id
+  env-name = "srhoton-prod"
 }
