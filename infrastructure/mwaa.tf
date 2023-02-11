b4610976be82a1d1e3bbf885a3069120f4887b20
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-02-11 17:52:03 -0500

    Move to tfvars

--- infrastructure/mwaa.tf
+++ infrastructure/mwaa.tf
@@ -1,13 +1,7 @@
-module "test_mwaa" {
+module "mwaa" {
   source = "git@github.com:srhoton/tf-module-mwaa.git"
   private_subnet_ids = module.default_network.private_subnet_list_mwaa
   vpc_id = module.default_network.vpc_id
-  env_name = "srhoton-test"
-}
-
-module "prod_mwaa" {
-  source = "git@github.com:srhoton/tf-module-mwaa.git"
-  private_subnet_ids = module.default_network.private_subnet_list_mwaa
-  vpc_id = module.default_network.vpc_id
-  env_name = "srhoton-prod"
+  env_name = var.env_name
+  base_cidr_block = var.base_cidr_block
 }
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,15 +1,7 @@
-output "test_dag_s3_path" {
+output "dag_s3_path" {
   value = module.test_mwaa.dag_s3_path
 }
 
-output "test_s3_arn" {
+output "s3_arn" {
   value = module.test_mwaa.s3_arn
-}
-
-output "prod_dag_s3_path" {
-  value = module.prod_mwaa.dag_s3_path
-}
-
-output "prod_s3_arn" {
-  value = module.prod_mwaa.s3_arn
 }
